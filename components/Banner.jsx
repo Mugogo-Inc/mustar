72ad67ab4e512fa8cc8f3eb2ed8ff7bd12034465
nzai <nzaih18@gmail.com>
2022-12-20 17:43:16 +0300

    banner edits

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,29 +1,39 @@
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import React from "react";
 import photo from "./images/safaris.jpg";
 import { BsArrowRightShort } from "react-icons/bs";
 
 const Banner = () => {
+  useEffect(() => {
+    console.log("mounted");
+    return () => {
+      console.log("unmounted");
+    };
+  }, []);
   return (
-    <div className="relative bg-gray-900 sm:flex">
-      <p className="absolute top-1/3 pl-3">
-        <div className="text-3xl  text-white">
-          <h1>
-            Ready for an Experience? <br />
-            Welcome to <br />
-            <span className="text-4xl font-bold text-orange-500">
-              {" "}
-              Zanzibar Experiences
-            </span>
+    <div className=" block sm:flex flex-row bg-gray-900 gap-6">
+      <div className="sm:w-1/2 relative">
+        <div className=" block absolute sm:top-20 left-3 text-3xl  text-white">
+          <h1 className="block z-40 mt-28 text-4xl sm:mt-0 font-medium">
+            Ready for an{" "}
+            <span className="text-5xl text-orange-500 font-semibold">
+              Experience
+            </span>{" "}
+            <br />
+            <span className="font-bold text-4xl">Karibu</span> <br />
+            <span className="text-6xl text-orange-600 font-bold">Zanzibar</span>
           </h1>
+          <button className="text-lg border-2 border-white outline-none p-2 mt-7 cursor-pointer ">
+            Explore Experiences
+            <BsArrowRightShort className="inline-block ml-2 text-2xl font-bold text-orange-600" />
+          </button>
         </div>
-        <button className=" border-2 p-3 mt-6 text-white">
-          {" "}
-          Explore Experiences
-          <BsArrowRightShort className="inline-block font-bold text-3xl text-orange-600" />
-        </button>
-      </p>
-      <Image src={photo} height="full" alt="jacugaz" />
+      </div>
+
+      <div className=" ">
+        {" "}
+        <Image src={photo} alt="glow up" height={900} width="full" />
+      </div>
     </div>
   );
 };
