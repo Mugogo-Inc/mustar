562d466fe8d284bee68f1e0501ec47507c46439b
nzai <nzaih18@gmail.com>
2022-12-22 10:28:08 +0300

    ff

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -18,13 +18,13 @@ const Gallery = ({ data }) => {
   return (
     <>
       <Head>
-        <Link
+        <link
           rel="stylesheet"
           type="text/css"
           charSet="UTF-8"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
         />
-        <Link
+        <link
           rel="stylesheet"
           type="text/css"
           href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
