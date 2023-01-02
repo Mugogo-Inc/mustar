7afa67507ed60bb283244379d559c10c3543e0d5
nzai <nzaih18@gmail.com>
2023-01-02 17:00:36 +0300

    kkk

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,6 @@ import React from "react";
 import Image from "next/image";
 import { footerImages } from "./tours/data";
 import Link from "next/link";
-import { dark } from "@mui/material/styles/createPalette";
 
 const Footer = () => {
   const date = new Date().getFullYear();
