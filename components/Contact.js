c2fc22d9f9b61715644690025641f2be9c197396
nzai <nzaih18@gmail.com>
2023-01-11 07:52:34 +0300

    tff

--- components/Contact.js
+++ components/Contact.js
@@ -7,7 +7,6 @@ const Contact = () => {
         <h1 className="text-4xl p-3 text-white">Contact Us</h1>
         <iframe
           src="https://whatsform.com/cTVIve"
-          sandbox=""
           width="100%"
           height="600"
           loading="lazy"
