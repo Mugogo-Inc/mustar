5dba86cc8f66a1aadfc4563c0815519d488c25ec
nzai <nzaih18@gmail.com>
2023-01-11 07:45:24 +0300

    iframe issues

--- components/Contact.js
+++ components/Contact.js
@@ -5,12 +5,13 @@ const Contact = () => {
     <div className="block m-auto text-center p-4 bg-gray-900" id="contact">
       <div>
         <h1 className="text-4xl p-3 text-white">Contact Us</h1>
-        {/* <iframe
+        <iframe
           src="https://whatsform.com/cTVIve"
+          sandbox=""
           width="100%"
           height="600"
-          className="bg-gray-900"
-        ></iframe> */}
+          loading="lazy"
+        ></iframe>
       </div>
     </div>
   );
---
+++ components/Iframe.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Iframe = () => {
+  return (
+    <iframe
+      src="https://whatsform.com/cTVIve"
+      sandbox=""
+      width="100%"
+      height="600"
+      loading="lazy"
+    ></iframe>
+  );
+};
+
+export default Iframe;
